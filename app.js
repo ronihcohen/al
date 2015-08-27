86e3980ca00880d7a316d1c1f0bd6469c09a16ee
ronihcohen@gmail.com <ronisM3>
2015-08-27 09:07:50 +0300

    Adding "getFeed" method.

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 var https = require('https');
 var config = require('./config.json');
 
-class AL {
-    constructor() {
+class FacebookGroup {
+    constructor(groupId) {
         this.clientId = config.clientId;
         this.clientSecret = config.clientSecret;
         this.token = '';
+        this.groupId = groupId;
     }
-    getFBToken() {
+    getToken() {
+        var path = '/v2.4/oauth/access_token?' +
+        'client_id='+this.clientId +
+        '&client_secret='+this.clientSecret +
+        '&grant_type=client_credentials';
+        return this.httpRec(path);
+    };
+    getFeed() {
+        return this.getToken().then((auth)=>{
+            this.token = auth.access_token;
+            var path = '/v2.4/'+ this.groupId + '/feed' +
+                '?access_token='+this.token;
+            return this.httpRec(path)
+        });
+    };
+    httpRec(path) {
         return new Promise((resolve, reject) => {
             https.get({
                 host: 'graph.facebook.com',
-                path: '/v2.4/oauth/access_token?client_id='+this.clientId +
-                '&client_secret='+this.clientSecret +
-                '&grant_type=client_credentials'
+                path: path
             }, (response) => {
                 var body = '';
                 response.on('data', (d) => {
@@ -21,9 +35,8 @@ class AL {
                 });
                 response.on('end', ()=> {
                     var parsed = JSON.parse(body);
-                    if (parsed.access_token){
-                        this.token = parsed.access_token;
-                        resolve(this.token);
+                    if (parsed){
+                        resolve(parsed);
                     } else {
                         reject();
                     }
@@ -33,9 +46,10 @@ class AL {
     };
 }
 
-var ALsession = new AL;
-ALsession.getFBToken().then((res) =>{
-    console.log(res);
-}).catch( (err) => {
-    console.log(err);
+var AL = new FacebookGroup('231120563712448');
+
+AL.getFeed().then((data) =>{
+   console.log(data);
+}).catch((reason)=>{
+   console.log(reason);
 });
