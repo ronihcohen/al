1e1021fc09de5f8f094f9cf3b96e38a03df23ef0
roni_hacohen <ronihcohen@gmail.com>
2015-08-28 00:44:24 +0300

    adding feedToDB method

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var https = require('https');
 var config = require('./config.json');
+var MongoClient = require('mongodb').MongoClient;
 
 class FacebookGroup {
     constructor(groupId) {
@@ -20,8 +21,9 @@ class FacebookGroup {
             this.token = auth.access_token;
             if (this.token){
                 var path = '/v2.4/'+ this.groupId + '/feed' +
-                    '?access_token='+this.token+
-                    '&fields=from,attachments,comments{attachment,from}';
+                    '?access_token='+this.token +
+                    '&fields=from,attachments,comments{attachment,from}' +
+                    '&limit=200';
                 return this.httpRec(path)
             } else {
                 return auth;
@@ -29,6 +31,7 @@ class FacebookGroup {
         });
     };
     httpRec(path) {
+        console.log('Request '+path);
         return new Promise((resolve, reject) => {
             https.get({
                 host: 'graph.facebook.com',
@@ -49,12 +52,47 @@ class FacebookGroup {
             });
         });
     };
+    feedToDB(feed){
+        setTimeout(()=>{
+            for (let post of feed.data){
+                this.collection.findOne({id: post.id}, (err,postInDb) => {
+                    if (!postInDb){
+                        this.collection.insert(post, (err, docs) =>  {
+                            console.log(docs);
+                            if (err){
+                                console.log(err);
+                            }
+                        });
+                    } else {
+                        console.log('Post ' + post.id + ' exist.');
+                    }
+                });
+            }
+
+            if (feed.paging){
+                let nextPage = feed.paging.next.substring(26);
+                this.httpRec(nextPage).then((nextPageFeed)=>{
+                    this.feedToDB(nextPageFeed)
+                });
+            } else {
+                this.db.close();
+            }
+
+        })
+    }
 }
 
 var AL = new FacebookGroup('231120563712448');
 
-AL.getFeed().then((data) =>{
-   console.log(data);
-}).catch((reason)=>{
-   console.log(reason);
+
+MongoClient.connect('mongodb://tardis.uk.to:27017/test',  (err, db) => {
+    if(err) throw err;
+    AL.collection = db.collection('test_insert');
+    AL.db = db;
+
+    AL.getFeed().then((feed) =>{
+         AL.feedToDB(feed);
+    }).catch((reason)=>{
+        console.log(reason);
+    });
 });
